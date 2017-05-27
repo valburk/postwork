41fbbdcf77028a8dc016804539c257cbb926b40e
Valerie Burk <valerieburk@aim.com>
2017-05-26 20:32:42 -0500

    Calculates tax with internal argument

--- tax.php
+++ tax.php
@@ -1,42 +1,26 @@
 <?php
 
-function pageController(){
-	$data = [];
-	if (isset($_GET['i'])){
-		$data['det'] = $_GET['i']; 
-	}else{
-		$data['det'] = NULL;
-	}
+function multiply($ogNumber){
 
-	if(isset($_GET['r'])){
-		$data['counter'] = $_GET['r'];
-	}else{
-		$data['counter'] = 0;
-	}	
+	$tax = $ogNumber * .07;
+	$added = $tax + $ogNumber;
+	return $added;
 
-	return $data;
 }
-extract(pageController());
 
-if($det === 'ping'){
-	$counter++;
-}elseif($det === 'pong'){
-	$counter--;
-}
+$calc = multiply(4);
 
   ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-    <title> Counter </title>
+    <title> Tax Calculator </title>
 </head>
 <body>
-	<h1> counter </h1>
+	<h1> Tax Calculator </h1>
     <h3>
- 		<?= $counter ?>
+<?= $calc ?>
  	</h3>
- 	<a href="/counter.php?i=ping&r=<?= $counter ?>">up</a>
- 	<a href="/counter.php?i=pong&r=<?= $counter ?>">down</a>
 </body>
 </html>
