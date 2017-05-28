578178f0a8544cc9422df0d041920598853e6f5b
Valerie Burk <valerieburk@aim.com>
2017-05-27 21:50:09 -0500

    Decided to use JS instead

--- tax.php
+++ tax.php
@@ -27,5 +27,25 @@ $calc = multiplyTax(4);
     
 <?= $calc ?>
  	</h3>
+<script>
+"use strict";
+
+ 	var operator = document.getElementById('middleInput');
+		var leftDisplay = document.getElementById('leftInput');
+		var rightDisplay = document.getElementById('rightInput'); 
+
+
+		function logOne(){
+            
+            	if (operator.innerText =="" && leftDisplay !==""){
+            		leftDisplay.innerText += "1";
+            	}else {
+            		rightDisplay.innerText += "1";
+            	}
+        }
+        document.getElementById('box-one').addEventListener('click', logOne);
+
+</script>
+
 </body>
 </html>
